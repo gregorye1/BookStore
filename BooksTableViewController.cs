0eb3799a3a2ce849274d33c7816aa57fe03bf995
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-24 12:56:22 -0600

    book defined class

--- BooksTableViewController.cs
+++ BooksTableViewController.cs
@@ -1,13 +1,25 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace BookStore
 {
     public partial class BooksTableViewController : UITableViewController
     {
+        List<Book> bookList;
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
+            bookList = new List<Book>(); 
         }
+    }
+
+    public class Book
+    {
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public int Year;
     }
 }
