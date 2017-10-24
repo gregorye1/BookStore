415bf7d135b819f87f90ae572977062139549d0d
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-24 13:45:42 -0600

    added detail

---
+++ BookTableViewCell.cs
@@ -0,0 +1,28 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace BookStore
+{
+    public partial class BookTableViewCell : UITableViewCell
+    {
+        private Book bookData;
+        public Book BookData
+        {
+            get { return bookData; }
+            set  
+            { 
+                bookData = value;
+
+                bookAuthorLabel.Text = bookData.Author;
+                bookNameLabel.Text = bookData.Name;
+                bookYearLabel.Text = bookData.Year.ToString();
+                bookPublisherLabel.Text = bookData.Publisher;
+            }
+        }
+
+        public BookTableViewCell (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
